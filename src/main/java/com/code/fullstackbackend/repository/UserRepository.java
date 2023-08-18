ea154c08550acf39df395324125f25d6942a0a71
Himanshu <himanshuthakre829@gmail.com>
2023-08-18 13:32:00 +0530

    First Commit

---
+++ src/main/java/com/code/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.code.fullstackbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.code.fullstackbackend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
